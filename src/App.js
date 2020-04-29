dac1bb95cade3e06cc6e97aaab747a5425a8bfd3
Alice Hendicott <alice.hendicott@gmail.com>
2020-04-29 20:27:05 +1000

    Add react routing to project
    Shows home page when path is "/" and shows game screen when a token is
    displayed as the path.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import React from "react";
 import "./App.css";
 import GameHeader from "./components/GameHeader";
 
+import { BrowserRouter, Route } from "react-router-dom";
+import GameBoard from "./screens/GameBoard";
+import Home from "./screens/Home";
+
 function App() {
-  return <GameHeader />;
+  return (
+    <>
+      <GameHeader />
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/:token" component={GameBoard} />
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
