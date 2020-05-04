d0b082dc682d7f4392eca1fe2e242b0ebee01839
Alice Hendicott <alice.hendicott@gmail.com>
2020-05-04 20:23:45 +1000

    Updates

---
+++ precache-manifest.1662f48f4acfe792d7d63277e4ee3afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5566bb30ae4969baf0e83579456505a9",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ae381308ab98d564bc72",
+    "url": "/tic-tac-toe/static/css/main.03bd0904.chunk.css"
+  },
+  {
+    "revision": "b2c5a194faa5d3f61f91",
+    "url": "/tic-tac-toe/static/js/2.e268853c.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/tic-tac-toe/static/js/2.e268853c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae381308ab98d564bc72",
+    "url": "/tic-tac-toe/static/js/main.276551e4.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  },
+  {
+    "revision": "5aa0d3a1160fc2d2bc54df2e4fcddbc2",
+    "url": "/tic-tac-toe/static/media/logo.5aa0d3a1.png"
+  }
+]);
